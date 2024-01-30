57809212325f0870b5a239109bbf3147206d5cf4
Beatriz Gabriela <beatriz.brandao@sptech.school>
2024-01-29 22:23:32 -0300

    PokeDex: Nova estrutura PokeApi, adição das informações faltantes no html

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,8 +1,21 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}limit=${limit}`;
+function convertPokemonIntoLi(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#${pokemon.number}</span> 
+    <span class="name">${pokemon.name}</span> 
+    <div class="detail">
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+        </ol>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+    </div>
+    </li>
+    `;
+}
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error))
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newList = pokemons.map(convertPokemonIntoLi).join('');
+    pokemonList.innerHTML = newList;
+})
