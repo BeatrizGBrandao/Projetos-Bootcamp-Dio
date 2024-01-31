572c9a53e0054e192dd3e63d7071e2bea020adc4
Beatriz Gabriela <beatriz.brandao@sptech.school>
2024-01-30 22:10:32 -0300

    PokeDex: Adicionando funcionalidade de paginação e cores de acordo com o tipo do Pokémon

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,21 +1,39 @@
-function convertPokemonIntoLi(pokemon) {
-    return `
-    <li class="pokemon">
-    <span class="number">#${pokemon.number}</span> 
-    <span class="name">${pokemon.name}</span> 
-    <div class="detail">
-        <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-        </ol>
-        <img src="${pokemon.photo}" alt="${pokemon.name}">
-    </div>
-    </li>
-    `;
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButtom = document.getElementById('loadMoreButton');
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newList = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span> 
+            <span class="name">${pokemon.name}</span> 
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+            </li>
+        `).join('');
+        pokemonList.innerHTML += newList;
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonItems(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newList = pokemons.map(convertPokemonIntoLi).join('');
-    pokemonList.innerHTML = newList;
+loadMoreButtom.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordWithNextPage = offset + limit;
+
+    if (qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItems(offset, newLimit);    
+        
+        loadMoreButtom.parentElement.removeChild(loadMoreButtom);
+    } else {
+        loadPokemonItems(offset, limit);
+    }
 })
